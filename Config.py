463106a0dd09841066097b19e2697cd49c8dde50
Aki Sasaki <asasaki@mozilla.com>
2010-05-25 11:45:31 -0700

    generic test.json, ParanoidConfig

--- Config.py
+++ Config.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 import pprint
+import sys
 try:
     import json
 except:
     import simplejson as json
 
+
+
+# BaseConfig {{{1
 class BaseConfig(object):
     """Basic config setting/getting.
     Debating whether to be paranoid about this stuff and put it all in
@@ -14,39 +18,134 @@ class BaseConfig(object):
     little heavy handed to go with as the default.
     """
     def __init__(self):
-        self.config = {}
+        pass
 
     def parseConfigFile(self, fileName):
-        """Read a config file as self.config.
+        """Read a config file and return a dictionary.
         TODO: read subsequent config files once self.config is already
         set, with options to override or drop conflicting config settings.
         """
         fh = open(fileName)
+        config = {}
         if fileName.endswith('.json'):
             jsonConfig = json.load(fh)
-            print json.dumps(jsonConfig, indent=2)
-            self.config = json.dumps(jsonConfig)
+            config = dict(jsonConfig)
         else:
+            contents = []
             for line in fh:
                 line = line[:-1]
-                config.append(line)
-                self.config = config
+                contents.append(line)
+                config = dict(contents)
         fh.close()
 
-    def dumpConfig(self, fileName=None):
+        """Return it here? Or set something?
+        """
+        return config
+
+    def queryConfig(self, varName=None):
+        if not varName:
+            return self.config
+        try:
+            str(varName) == varName
+        except:
+            """It would be cool to allow for dictionaries here, to specify
+            which subset(s) of config to return
+            """
+            pass
+        else:
+            if varName in self.config:
+                return self.config[varName]
+
+    def setConfig(self, config):
+        """It would be good to detect if self.config is already set, and
+        if so, have settings as to how to determine what overrides what.
+        """
+        self.config = config
+
+    def queryVar(self, varName):
+        return self.queryConfig(varName=varName)
+
+    def setVar(self, varName, value):
+        self.config[varName] = value
+
+    def dumpConfig(self, config=None, fileName=None):
         """Dump the configuration somewhere, default to STDOUT.
         Be nice to be able to write a .py or .json file according to
         filename.
         """
-        pp = pprint(PrettyPrinter(indent=2))
-        pp.pprint(self.config)
+        if not config:
+            config = self.queryConfig()
+        if not fileName:
+            pp = pprint.PrettyPrinter(indent=2, width=10)
+            pp.pprint(config)
 
     def parseArgs(self):
         pass
 
 
 
+class SimpleConfig(BaseConfig):
+    def __init__(self):
+        self.config = {}
+
+
+
+# ParanoidConfig {{{1
+class ParanoidConfig(BaseConfig):
+    def __init__(self):
+        self._config = {}
+        self._configLock = False
+        BaseConfig.__init__(self)
+
+    def lockConfig(self):
+        self._configLock = True
+
+    def _checkConfigLock(self):
+        if self._configLock:
+            print "FATAL: ParanoidConfig is locked! Exiting..."
+            sys.exit(-1)
+
+    def queryConfig(self, varName=None):
+        if not varName:
+            return self._config
+        try:
+            str(varName) == varName
+        except:
+            """It would be cool to allow for dictionaries here, to specify
+            which subset(s) of config to return
+            """
+            pass
+        else:
+            if varName in self._config:
+                return self._config[varName]
+
+    def setConfig(self, config):
+        self._checkConfigLock()
+        self._config = config
+
+    def setVar(self, varName, value):
+        self._checkConfigLock()
+        self._config[varName] = value
+
+
+
+# __main__ {{{1
 if __name__ == '__main__':
     obj = BaseConfig()
-    obj.parseConfigFile('configs/test/test.json')
-    obj.dumpConfig
+    config=obj.parseConfigFile('configs/test/test.json')
+    obj.dumpConfig(config)
+    if config['key1'] != "value1":
+        print "ERROR key1 isn't value1!"
+
+    obj = ParanoidConfig()
+    obj.setConfig(obj.parseConfigFile('configs/test/test.json'))
+    obj.lockConfig()
+    if obj.queryConfig('key1') != "value1":
+        print "ERROR key1 isn't value1!"
+    try:
+        print "This should fail: with a FATAL message"
+        obj.setVar('thisShouldFail', 'miserably')
+    except:
+        print "Yay!"
+    else:
+        print "Gah. ParanoidConfig is broken."
