fc3c621e02f15a34989eac397da69ffbed2a2e11
Aki Sasaki <asasaki@mozilla.com>
2010-08-02 17:03:00 -0700

    try copying the mobile-l10n.js in instead of doing the two-part deb extraction

--- scripts/multil10n.py
+++ scripts/multil10n.py
@@ -538,13 +538,13 @@ class MaemoMultiLocaleRepack(MultiLocaleRepack):
         absTmpDebDir = os.path.join(absObjdir, tmpDebDir)
 
         # TODO error checking
-        command = "make package AB_CD=multi"
-        self._processCommand(command=command, cwd=absObjdir)
-        command = "make deb AB_CD=multi"
-        self._processCommand(command=command, cwd=absObjdir)
-
-        # Ugh, get the binary bits from the en-US deb, and the multilocale
-        # bits from the multi deb
+#        command = "make package AB_CD=multi"
+#        self._processCommand(command=command, cwd=absObjdir)
+#        command = "make deb AB_CD=multi"
+#        self._processCommand(command=command, cwd=absObjdir)
+#
+#        # Ugh, get the binary bits from the en-US deb, and the multilocale
+#        # bits from the multi deb
         self.rmtree(os.path.join(absTmpDebDir))
         self.mkdir_p(os.path.join(absTmpDebDir, "DEBIAN"))
         arErrorRegex = [{
@@ -562,6 +562,13 @@ class MaemoMultiLocaleRepack(MultiLocaleRepack):
           (debName, tmpDebDir)
         self.runCommand(command=command, cwd=absObjdir,
                         errorRegex=arErrorRegex)
+
+        command = "find %s -name mobile-l10n.js" % tmpDebDir
+        mobileL10nFile = self.getOutputFromCommand(command=command,
+                                                   cwd=absObjdir)
+        command = "cp ../mobile/installer/mobile-l10n.js %s" % mobileL10nFile
+        self.runCommand(command=command, cwd=absObjdir, haltOnFailure=True)
+
         command = "dpkg-deb -b %s dist/%s" % (absTmpDebDir, debName)
         self._processCommand(command=command, cwd=absObjdir)
 
