26a56da35c1a9e0f12dc77a0d938a921231c91e7
Aki Sasaki <asasaki@mozilla.com>
2010-10-01 12:26:32 -0700

    more msg="..." in test_script.py

--- test/test_script.py
+++ test/test_script.py
@@ -26,22 +26,35 @@ class TestScript(unittest.TestCase):
                 shutil.rmtree('test_dir')
             else:
                 os.remove('test_dir')
-        self.assertFalse(os.path.exists('test_dir'))
+        self.assertFalse(os.path.exists('test_dir'),
+                         msg="testHelperFunctions() cleanup failed")
         s.mkdir_p('test_dir/foo/bar/baz')
-        self.assertTrue(os.path.isdir('test_dir/foo/bar/baz'))
+        self.assertTrue(os.path.isdir('test_dir/foo/bar/baz'),
+                        msg="mkdir_p error")
+        s.mkdir_p('test_dir/foo/bar/baz')
+        self.assertTrue(os.path.isdir('test_dir/foo/bar/baz'),
+                        msg="mkdir_p error when dir exists")
         s.downloadFile("http://www.google.com", file_name="test_dir/google",
                        error_level="ignore")
-        self.assertTrue(os.path.exists('test_dir/google'))
+        self.assertTrue(os.path.exists('test_dir/google'),
+                        msg="error downloading google.com")
         contents1 = s.getOutputFromCommand("cat test_dir/google")
         fh = open("test_dir/google")
         contents2 = fh.read()
         fh.close()
-        self.assertEqual(contents1, contents2)
-        self.assertEqual(s.runCommand("cat google", cwd="test_dir"), 0)
+        self.assertEqual(contents1, contents2,
+                         msg="getOutputFromCommand('cat file') differs from fh.read")
+        self.assertEqual(s.runCommand("cat google", cwd="test_dir"), 0,
+                         msg="runCommand('cat file') did not exit 0")
         s.runCommand("rm test_dir/google")
-        self.assertFalse(os.path.exists('test_dir/google'))
+        self.assertFalse(os.path.exists('test_dir/google'),
+                         msg="runCommand('rm file') did not remove file")
         s.rmtree('test_dir')
-        self.assertFalse(os.path.exists('test_dir'))
+        self.assertFalse(os.path.exists('test_dir'),
+                         msg="rmtree unsuccessful")
+        s.rmtree('test_dir')
+        self.assertFalse(os.path.exists('test_dir'),
+                         msg="2nd rmtree unsuccessful")
         self.cleanup()
 
     def testSummary(self):
@@ -49,19 +62,24 @@ class TestScript(unittest.TestCase):
         s = script.BaseScript(config={'log_type': 'multi'},
                               initial_config_file='test/test.json')
         info_logsize = os.path.getsize("test_logs/test_info.log")
-        self.assertTrue(info_logsize > 0)
+        self.assertTrue(info_logsize > 0,
+                        msg="initial info logfile missing/size 0")
         s.addSummary('one')
         info_logsize2 = os.path.getsize("test_logs/test_info.log")
-        self.assertTrue(info_logsize < info_logsize2)
+        self.assertTrue(info_logsize < info_logsize2,
+                        msg="addSummary() not logged")
         warning_logsize = os.path.getsize("test_logs/test_warning.log")
         s.addSummary('two', level="warning")
         warning_logsize2 = os.path.getsize("test_logs/test_warning.log")
-        self.assertTrue(warning_logsize < warning_logsize2)
+        self.assertTrue(warning_logsize < warning_logsize2,
+                     msg="addSummary(level='warning') not logged in warning log")
         info_logsize = os.path.getsize("test_logs/test_info.log")
         warning_logsize = os.path.getsize("test_logs/test_warning.log")
         s.summary()
         info_logsize2 = os.path.getsize("test_logs/test_info.log")
         warning_logsize2 = os.path.getsize("test_logs/test_warning.log")
-        self.assertTrue(info_logsize < info_logsize2)
-        self.assertTrue(warning_logsize < warning_logsize2)
+        self.assertTrue(info_logsize < info_logsize2,
+                        msg="summary() didn't log to info")
+        self.assertTrue(warning_logsize < warning_logsize2,
+                        msg="summary() with warning didn't log to warning")
         self.cleanup()
