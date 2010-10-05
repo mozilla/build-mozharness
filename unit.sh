cbd650ec862b50b262b7e6ffa9207354614de567
Aki Sasaki <asasaki@mozilla.com>
2010-10-04 19:51:15 -0700

    mroe coverage

--- unit.sh
+++ unit.sh
@@ -5,6 +5,12 @@ pylint -E -e F -f parseable lib/*.py scripts/*.py
 
 if [ -e localconfig.json ] ; then rm localconfig.json; fi
 coverage run -a --branch --omit='/Library/*,/usr/*,/opt/*' `which nosetests`
+for filename in lib/*.py ; do
+  coverage run -a --branch --omit='/Library/*,/usr/*,/opt/*' $filename
+done
+for filename in scripts/*.py ; do
+  coverage run -a --branch --omit='/Library/*,/usr/*,/opt/*' $filename --list-actions
+done
 if [ -e localconfig.json ] ; then rm localconfig.json; fi
 
 coverage html --omit="/Library/*,/usr/*,/opt/*" -d coverage.new
