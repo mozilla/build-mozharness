ed657c9dea396c6a90ea8be09e8ae51ee5b64de0
Aki Sasaki <asasaki@mozilla.com>
2011-02-10 13:43:05 -0800

    default to /builds/scratchbox/...

--- scripts/maemo_multi_locale_build.py
+++ scripts/maemo_multi_locale_build.py
@@ -39,14 +39,14 @@ class MaemoMultiLocaleBuild(MultiLocaleBuild):
      {"action": "store",
       "dest": "sbox_home",
       "type": "string",
-      "default": "/scratchbox/users/cltbld/home/cltbld/",
+      "default": "/builds/scratchbox/users/cltbld/home/cltbld/",
       "help": "Specify the scratchbox user home directory"
      }
     ],[
      ["--sbox-root",],     {"action": "store",
       "dest": "sbox_root",
       "type": "string",
-      "default": "/scratchbox/users/cltbld",      "help": "Specify the scratchbox user home directory"
+      "default": "/builds/scratchbox/users/cltbld",      "help": "Specify the scratchbox user home directory"
      }
     ],[
      ["--sbox_path",],
