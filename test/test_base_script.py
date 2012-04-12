245fd8ffc6ace8e37a5343150cd92c38f702833b
Aki Sasaki <asasaki@mozilla.com>
2012-04-12 15:42:42 -0700

    bug 743856 - fix get_output_from_command noop test. r=jhammel

--- test/test_base_script.py
+++ test/test_base_script.py
@@ -84,7 +84,7 @@ class TestScript(unittest.TestCase):
     def test_noop_get_output_from_command(self):
         self.s = get_noop_script_obj()
         contents1 = self.s.get_output_from_command("cat test/test.json", cwd="configs")
-        self.assertEqual(contents1, None,
+        self.assertEqual(contents1, '',
                          msg="get_output_from_command noop error")
 
     def test_noop_run_command(self):
