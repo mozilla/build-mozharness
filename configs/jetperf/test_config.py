4a98077f9f994ae1ae82b8a6dfab6da254242b54
Jeff Hammel <jhammel@mozilla.com>
2012-09-07 15:47:38 -0700

    Bug 786885 - jetperf script doesnt work on windows;r=aki

--- configs/jetperf/test_config.py
+++ configs/jetperf/test_config.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from datetime import datetime
 
 config = {
@@ -15,3 +16,17 @@ config = {
         'baseline',
         ],
 }
+
+# need to specify pywin32 location for windows
+# https://bugzilla.mozilla.org/show_bug.cgi?id=786885
+# logic from http://hg.mozilla.org/build/talos/file/1c5976f92643/setup.py
+if os.name == 'nt':
+    base_link = "http://superb-sea2.dl.sourceforge.net/project/pywin32/pywin32/Build216/pywin32-216.%s-py%s.exe"
+    python_version = '%d.%d' % sys.version_info[0:2]
+    if sys.maxsize > 2**32:  # is 64bits?
+        platform_name = 'win-amd64'
+    else:
+        platform_name = 'win32'
+    config['pywin32_url'] = base_link % (platform_name, python_version)
+    config['virtualenv_modules'] = ['pywin32', 'talos', 'mozinstall']
+    print 'On windows: %s' % config
