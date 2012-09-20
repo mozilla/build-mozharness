08e7b36d88c87226783b361ecc6cecbd32995f3b
Aki Sasaki <asasaki@mozilla.com>
2012-09-20 16:09:24 -0700

    bug 789520 - rmtree doesn't work reliably on windows. r=jhammel

--- mozharness/base/script.py
+++ mozharness/base/script.py
@@ -61,7 +61,8 @@ class OSMixin(object):
             if not self.config.get('noop'):
                 if os.path.isdir(path):
                     if self._is_windows():
-                        self._rmdir_recursive(path)
+                        # bug 789520: using rmdir /s instead of _rmdir_recursive
+                        self.run_command(['rmdir', '/S', path])
                     else:
                         shutil.rmtree(path)
                 else:
