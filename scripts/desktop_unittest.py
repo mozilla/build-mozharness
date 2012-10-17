4ec39b8f4d2fd081dd8b6c8047f173623a8df105
Aki Sasaki <asasaki@mozilla.com>
2012-10-17 13:42:31 -0700

    bug 802796 - cedar mozharness unittests on osx 10.8 are perma-red. r=jgriffin

--- scripts/desktop_unittest.py
+++ scripts/desktop_unittest.py
@@ -188,6 +188,7 @@ class DesktopUnittest(TestingMixin, MercurialScript):
         {'mozlog': os.path.join('tests', 'mozbase', 'mozlog')},
         {'mozinfo': os.path.join('tests', 'mozbase', 'mozinfo')},
         {'mozhttpd': os.path.join('tests', 'mozbase', 'mozhttpd')},
+        {'mozfile': os.path.join('tests', 'mozbase', 'mozfile')},
         {'mozinstall': os.path.join('tests', 'mozbase', 'mozinstall')},
         {'manifestdestiny': os.path.join('tests', 'mozbase', 'manifestdestiny')},
         {'mozprofile': os.path.join('tests', 'mozbase', 'mozprofile')},
