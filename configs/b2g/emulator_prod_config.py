e24b4c6a9566c341aa8fafad8439b67b7b34dc51
Jonathan Griffin <jgriffin@mozilla.com>
2012-10-24 14:58:43 -0700

    Bug 804846 - Update xpcshell url in b2g unittest config, r=aki

--- configs/b2g/emulator_prod_config.py
+++ configs/b2g/emulator_prod_config.py
@@ -4,7 +4,7 @@ config = {
     # mozharness options
     "application": "b2g",
     "emulator_url": "http://runtime-binaries.pvt.build.mozilla.org/tooltool/sha512/66f9f7f58a100b67d151e64d4c9069dc06105cbf08a10170c341100154c4999708f0304530d7adffbc0ce84c18cdd9f2b49b21365fa8f08b73027e424c4ce68b",
-    "xpcshell_url": "http://runtime-binaries.pvt.build.mozilla.org/tooltool/sha512/7c349ae926492bd67bd861f23013d5dd1ad14966bc5da314c6036ddd8db53c119db909a6a22718a0794c56d1f2d8c1742be067489f29e2066d9a5eee823b403a",
+    "xpcshell_url": "http://runtime-binaries.pvt.build.mozilla.org/tooltool/sha512/d4297e762649b174070a33d039fd062edd9f29a751650f0508327a6cf366b3a35fe24e7cd0f7b728d74f7d15399f9c1adc5b178e5803a3a66bfce7a8dcd62daa",
 
     "exes": {
         'python': '/tools/buildbot/bin/python',
