4f25802611149a62950e913a2dfad4c9788aa00b
Chris AtLee <catlee@mozilla.com>
2012-11-07 07:32:47 -0800

    nobug - don't need to hit clobberer for try builds. r=rail
    --HG--
    extra : rebase_source : 66ae060752520658d87133ad7eb58a5f473b9c84

--- configs/b2g/releng-try.py
+++ configs/b2g/releng-try.py
@@ -32,7 +32,7 @@ config = {
     },
     "purge_minsize": 10,
     #"clobberer_url": "http://clobberer-stage.pvt.build.mozilla.org/index.php",
-    "clobberer_url": "http://clobberer.pvt.build.mozilla.org/index.php",
+    #"clobberer_url": "http://clobberer.pvt.build.mozilla.org/index.php",
     "is_automation": True,
     "force_clobber": True,
 }
