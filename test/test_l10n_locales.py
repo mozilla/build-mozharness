ccb7ecc68f4851099e4318b905db9a778c20a5ce
Aki Sasaki <asasaki@mozilla.com>
2012-11-13 13:34:26 -0800

    bug 793709 - update tests for b264b7808aff. test only

--- test/test_l10n_locales.py
+++ test/test_l10n_locales.py
@@ -26,7 +26,7 @@ class LocalesTest(locales.LocalesMixin, script.BaseScript):
         self.log_obj = None
 
 class TestLocalesMixin(unittest.TestCase):
-    BASE_ABS_DIRS = ['abs_compare_locales_dir', 'abs_log_dir',
+    BASE_ABS_DIRS = ['base_work_dir', 'abs_compare_locales_dir', 'abs_log_dir',
                      'abs_upload_dir', 'abs_work_dir']
     def setUp(self):
         cleanup()
