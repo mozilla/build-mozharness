2c9ef603057bb6357d51ad7286f9b61a310612d2
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2012-12-05 11:31:48 -0500

    Bug 802317. Use version-agnostic buildbot to trigger sendchanges. r=Callek

--- configs/b2g/releng.py
+++ configs/b2g/releng.py
@@ -26,7 +26,7 @@ config = {
     "sendchange_masters": ["buildbot-master36.build.mozilla.org:9301"],
     "exes": {
         "tooltool.py": "/tools/tooltool.py",
-        "buildbot": "/tools/buildbot-0.8.4-pre-moz2/bin/buildbot",
+        "buildbot": "/tools/buildbot/bin/buildbot",
     },
     "env": {
         "CCACHE_DIR": "/builds/ccache",
