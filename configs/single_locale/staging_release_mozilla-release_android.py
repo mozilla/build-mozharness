861be2dabdf20d57ee1aee89506262c591741d49
kmoir@mozilla.com <kmoir@mozilla.com>
2013-01-05 10:27:07 -0500

    Bug 814191 - [merge day] update android single locale mozilla-release 18.0 config for mock r=aki

--- configs/single_locale/staging_release_mozilla-release_android.py
+++ configs/single_locale/staging_release_mozilla-release_android.py
@@ -74,4 +74,17 @@ config = {
         "upload-repacks",
         "summary",
     ],
+    # Mock
+    "mock_target": "mozilla-centos6-i386",
+    "mock_packages": [
+        "autoconf213", "mozilla-python27-mercurial", "ccache",
+        "android-sdk15", "android-sdk16", "android-ndk5", "zip",
+        "java-1.6.0-openjdk-devel", "zlib-devel", "glibc-static",
+        "openssh-clients", "mpfr", "wget",
+         ],
+    "mock_files": [
+        ("/home/cltbld/.ssh", "/home/mock_mozilla/.ssh"),
+        ("/home/cltbld/.android", "/builds/.android"),
+        ("/home/cltbld/.mozpass.cfg", "/builds/.mozpass.cfg"),
+    ],
 }
