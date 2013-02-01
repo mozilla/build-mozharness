0857967246a4be8e71fce7cbf20f3b687d29d53c
Aki Sasaki <asasaki@mozilla.com>
2013-02-01 10:13:23 -0800

    bug 837006 - use pymake for win32 b2g desktop builds. r=bhearsum

--- test/test_base_script.py
+++ test/test_base_script.py
@@ -1,15 +1,7 @@
 import gc
 import os
 import re
-import shutil
-import subprocess
-import sys
 import unittest
-
-try:
-    import simplejson as json
-except ImportError:
-    import json
 
 import mozharness.base.errors as errors
 import mozharness.base.log as log
@@ -20,11 +12,13 @@ test_string = '''foo
 bar
 baz'''
 
+
 class CleanupObj(script.OSMixin, log.LogMixin):
     def __init__(self):
         super(CleanupObj, self).__init__()
         self.log_obj = None
         self.config = {'log_level': ERROR}
+
 
 def cleanup():
     gc.collect()
@@ -34,11 +28,13 @@ def cleanup():
     for f in ('test_logs', 'test_dir', 'tmpfile_stdout', 'tmpfile_stderr'):
         c.rmtree(f)
 
+
 def get_debug_script_obj():
     s = script.BaseScript(config={'log_type': 'multi',
                                   'log_level': DEBUG},
                           initial_config_file='test/test.json')
     return s
+
 
 class TestScript(unittest.TestCase):
     def setUp(self):
@@ -85,7 +81,7 @@ class TestScript(unittest.TestCase):
 
     def test_log(self):
         self.s = get_debug_script_obj()
-        self.s.log_obj=None
+        self.s.log_obj = None
         self._test_log_helper(self.s)
         del(self.s)
         self.s = script.BaseScript(initial_config_file='test/test.json')
@@ -94,8 +90,8 @@ class TestScript(unittest.TestCase):
     def test_run_nonexistent_command(self):
         self.s = get_debug_script_obj()
         self.s.run_command(command="this_cmd_should_not_exist --help",
-                      env={'GARBLE': 'FARG'},
-                      error_list=errors.PythonErrorList)
+                           env={'GARBLE': 'FARG'},
+                           error_list=errors.PythonErrorList)
         error_logsize = os.path.getsize("test_logs/test_error.log")
         self.assertTrue(error_logsize > 0,
                         msg="command not found error not hit")
@@ -103,45 +99,45 @@ class TestScript(unittest.TestCase):
     def test_run_command_in_bad_dir(self):
         self.s = get_debug_script_obj()
         self.s.run_command(command="ls",
-                      cwd='/this_dir_should_not_exist',
-                      error_list=errors.PythonErrorList)
+                           cwd='/this_dir_should_not_exist',
+                           error_list=errors.PythonErrorList)
         error_logsize = os.path.getsize("test_logs/test_error.log")
         self.assertTrue(error_logsize > 0,
                         msg="bad dir error not hit")
 
     def test_get_output_from_command_in_bad_dir(self):
         self.s = get_debug_script_obj()
-        output = self.s.get_output_from_command(command="ls",
-                     cwd='/this_dir_should_not_exist')
+        self.s.get_output_from_command(command="ls", cwd='/this_dir_should_not_exist')
         error_logsize = os.path.getsize("test_logs/test_error.log")
         self.assertTrue(error_logsize > 0,
                         msg="bad dir error not hit")
 
     def test_get_output_from_command_with_missing_file(self):
         self.s = get_debug_script_obj()
-        output = self.s.get_output_from_command(command="ls /this_file_should_not_exist")
+        self.s.get_output_from_command(command="ls /this_file_should_not_exist")
         error_logsize = os.path.getsize("test_logs/test_error.log")
         self.assertTrue(error_logsize > 0,
                         msg="bad file error not hit")
 
-    def test_get_output_from_command_with_missing_file(self):
+    def test_get_output_from_command_with_missing_file2(self):
         self.s = get_debug_script_obj()
-        self.s.run_command(command="cat mozharness/base/errors.py",
-                      error_list=[{
-                       'substr': "error", 'level': ERROR
-                      },{
-                       'regex': re.compile(',$'), 'level': IGNORE,
-                      },{
-                       'substr': ']$', 'level': WARNING,
-                      }])
+        self.s.run_command(
+            command="cat mozharness/base/errors.py",
+            error_list=[{
+                'substr': "error", 'level': ERROR
+            }, {
+                'regex': re.compile(',$'), 'level': IGNORE,
+            }, {
+                'substr': ']$', 'level': WARNING,
+            }])
         error_logsize = os.path.getsize("test_logs/test_error.log")
         self.assertTrue(error_logsize > 0,
                         msg="error list not working properly")
 
 
-
 class TestHelperFunctions(unittest.TestCase):
     temp_file = "test_dir/mozilla"
+
     def setUp(self):
         cleanup()
         self.s = None
@@ -176,7 +172,7 @@ class TestHelperFunctions(unittest.TestCase):
         self.s = script.BaseScript(initial_config_file='test/test.json')
         temp_file_name = os.path.basename(self.temp_file)
         self.assertEqual(self.s.run_command("cat %s" % temp_file_name,
-                                       cwd="test_dir"), 0,
+                                            cwd="test_dir"), 0,
                          msg="run_command('cat file') did not exit 0")
 
     def test_move1(self):
@@ -202,7 +198,7 @@ class TestHelperFunctions(unittest.TestCase):
         self.s.copyfile(self.temp_file, temp_file2)
         self.assertEqual(os.path.getsize(self.temp_file),
                          os.path.getsize(temp_file2),
-                         msg="%s and %s are different sizes after copyfile()" % \
+                         msg="%s and %s are different sizes after copyfile()" %
                              (self.temp_file, temp_file2))
 
     def test_existing_rmtree(self):
@@ -259,6 +255,25 @@ class TestHelperFunctions(unittest.TestCase):
         script_env = self.s.query_env(partial_env={'PATH': partial_path})
         self.assertEqual(script_env['PATH'], full_path)
 
+    def test_query_exe(self):
+        self.s = script.BaseScript(
+            initial_config_file='test/test.json',
+            config={'exes': {'foo': 'bar'}},
+        )
+        path = self.s.query_exe('foo')
+        self.assertEqual(path, 'bar')
+
+    def test_query_exe_string_replacement(self):
+        self.s = script.BaseScript(
+            initial_config_file='test/test.json',
+            config={
+                'base_work_dir': 'foo',
+                'work_dir': 'bar',
+                'exes': {'foo': os.path.join('%(abs_work_dir)s', 'baz')},
+            },
+        )
+        path = self.s.query_exe('foo')
+        self.assertEqual(path, os.path.join('foo', 'bar', 'baz'))
 
 
 class TestScriptLogging(unittest.TestCase):
@@ -274,14 +289,14 @@ class TestScriptLogging(unittest.TestCase):
 
     def test_info_logsize(self):
         self.s = script.BaseScript(config={'log_type': 'multi'},
-                              initial_config_file='test/test.json')
+                                   initial_config_file='test/test.json')
         info_logsize = os.path.getsize("test_logs/test_info.log")
         self.assertTrue(info_logsize > 0,
                         msg="initial info logfile missing/size 0")
 
     def test_add_summary_info(self):
         self.s = script.BaseScript(config={'log_type': 'multi'},
-                              initial_config_file='test/test.json')
+                                   initial_config_file='test/test.json')
         info_logsize = os.path.getsize("test_logs/test_info.log")
         self.s.add_summary('one')
         info_logsize2 = os.path.getsize("test_logs/test_info.log")
@@ -290,7 +305,7 @@ class TestScriptLogging(unittest.TestCase):
 
     def test_add_summary_warning(self):
         self.s = script.BaseScript(config={'log_type': 'multi'},
-                              initial_config_file='test/test.json')
+                                   initial_config_file='test/test.json')
         warning_logsize = os.path.getsize("test_logs/test_warning.log")
         self.s.add_summary('two', level=WARNING)
         warning_logsize2 = os.path.getsize("test_logs/test_warning.log")
@@ -299,7 +314,7 @@ class TestScriptLogging(unittest.TestCase):
 
     def test_summary(self):
         self.s = script.BaseScript(config={'log_type': 'multi'},
-                              initial_config_file='test/test.json')
+                                   initial_config_file='test/test.json')
         self.s.add_summary('one')
         self.s.add_summary('two', level=WARNING)
         info_logsize = os.path.getsize("test_logs/test_info.log")
@@ -316,7 +331,7 @@ class TestScriptLogging(unittest.TestCase):
 
     def _test_log_level(self, log_level, log_level_file_list):
         self.s = script.BaseScript(config={'log_type': 'multi'},
-                              initial_config_file='test/test.json')
+                                   initial_config_file='test/test.json')
         if log_level != FATAL:
             self.s.log('testing', level=log_level)
         else:
