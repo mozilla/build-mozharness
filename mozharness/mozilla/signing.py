8753c469ab3fafd6dfe423603f04cf6a27c93d1b
Ben Hearsum <bhearsum@mozilla.com>
2013-03-21 09:02:14 -0400

    bug 853036: use uppercase SHA512 in all snippets / balrog blobs. r=nthomas

--- mozharness/mozilla/signing.py
+++ mozharness/mozilla/signing.py
@@ -35,7 +35,7 @@ AndroidSignatureVerificationErrorList = BaseErrorList + [{
 SNIPPET_TEMPLATE = """version=1
 type=complete
 url=%(url)s
-hashFunction=sha512
+hashFunction=SHA512
 hashValue=%(sha512_hash)s
 size=%(size)d
 build=%(buildid)s
