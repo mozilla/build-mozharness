6a8b61aa9cfd9152371309ffd61ff70fbceadfe0
Ben Hearsum <bhearsum@mozilla.com>
2013-05-20 12:39:52 -0400

    bug 863277: bring up servo buildbot/bors master. r=aki
    --HG--
    branch : production

---
+++ configs/servo/linux.py
@@ -0,0 +1,5 @@
+config = {
+    'mock_target': 'mozilla-centos6-x86_64',
+    'mock_packages': ['freetype-devel', 'fontconfig-devel', 'glib2-devel', 'autoconf213', 'git', 'make', 'libX11-devel', 'mesa-libGL-devel', 'freeglut-devel'],
+    'mock_files': [('/home/servobld/.ssh', '/home/mock_mozilla/.ssh')],
+}
