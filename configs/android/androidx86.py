ccc8b2a10dfffb7ca87c5d2fb39872b14e05c3b8
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2013-09-05 14:08:15 -0400

    Bug 895186. Androidx86 missing config file. r=aki

---
+++ configs/android/androidx86.py
@@ -0,0 +1,191 @@
+import os
+
+config = {
+    "buildbot_json_path": "buildprops.json",
+    "host_utils_url": "http://bm-remote.build.mozilla.org/tegra/tegra-host-utils.Linux.742597.zip",
+    "fennec_package_name": "org.mozilla.fennec",
+    "robocop_package_name": "org.mozilla.roboexample.test",
+    "device_ip": "127.0.0.1",
+    "default_sut_port1": "20701",
+    "default_sut_port2": "20700", # does not prompt for commands
+    "avds_path": "/home/cltbld/avds/test-x86.tar.gz",
+    ".avds_dir": "/home/cltbld/.android/avd",
+    "exes": {
+        'adb': '/tools/android-sdk18/platform-tools/adb',
+        'python': '/tools/buildbot/bin/python',
+        'virtualenv': ['/tools/buildbot/bin/python', '/tools/misc-python/virtualenv.py'],
+    },
+    "env": {
+        "DISPLAY": ":0.0",
+        "PATH": "%(PATH)s:/tools/android-sdk18/tools:/tools/android-sdk18/platform-tools"
+    },
+    "default_actions": [
+        'clobber',
+        'read-buildbot-config',
+        'setup-avds',
+        'start-emulators',
+        'download-and-extract',
+        'create-virtualenv',
+        'install',
+        'run-tests',
+        'stop-emulators',
+    ],
+    "emulators": [
+        {
+            "name": "test-x86-1",
+            "device_id": "emulator-5554",
+            "http_port": "8854", # starting http port to use for the mochitest server
+            "ssl_port": "4454", # starting ssl port to use for the server
+            "emulator_port": 5554,
+            "sut_port1": 20701,
+            "sut_port2": 20700
+        },
+        {
+            "name": "test-x86-2",
+            "device_id": "emulator-5556",
+            "http_port": "8856", # starting http port to use for the mochitest server
+            "ssl_port": "4456", # starting ssl port to use for the server
+            "emulator_port": 5556,
+            "sut_port1": 20703,
+            "sut_port2": 20702
+        },
+        {
+            "name": "test-x86-3",
+            "device_id": "emulator-5558",
+            "http_port": "8858", # starting http port to use for the mochitest server
+            "ssl_port": "4458", # starting ssl port to use for the server
+            "emulator_port": 5558,
+            "sut_port1": 20705,
+            "sut_port2": 20704
+        },
+        {
+            "name": "test-x86-4",
+            "device_id": "emulator-5560",
+            "http_port": "8860", # starting http port to use for the mochitest server
+            "ssl_port": "4460", # starting ssl port to use for the server
+            "emulator_port": 5560,
+            "sut_port1": 20707,
+            "sut_port2": 20706
+        }
+    ],
+    "emulator_components": {
+        "kernel_path": "/home/cltbld/.android/avd/kernel-qemu",
+        "system_image_path": "/home/cltbld/.android/avd/system.img",
+        "ramdisk_path": "/home/cltbld/.android/avd/ramdisk.img"
+    },
+    "test_suite_definitions": {
+        "mochitest-1": {
+            "category": "mochitest",
+            "extra_args": ["--total-chunks", "8", "--this-chunk", "1", "--run-only-tests", "androidx86.json"],
+        },
+        "mochitest-2": {
+            "category": "mochitest",
+            "extra_args": ["--total-chunks", "8", "--this-chunk", "2", "--run-only-tests", "androidx86.json"],
+        },
+        "mochitest-3": {
+            "category": "mochitest",
+            "extra_args": ["--total-chunks", "8", "--this-chunk", "3", "--run-only-tests", "androidx86.json"],
+        },
+        "mochitest-4": {
+            "category": "mochitest",
+            "extra_args": ["--total-chunks", "8", "--this-chunk", "4", "--run-only-tests", "androidx86.json"],
+        },
+        "mochitest-5": {
+            "category": "mochitest",
+            "extra_args": ["--total-chunks", "8", "--this-chunk", "5", "--run-only-tests", "androidx86.json"],
+        },
+        "mochitest-6": {
+            "category": "mochitest",
+            "extra_args": ["--total-chunks", "8", "--this-chunk", "6", "--run-only-tests", "androidx86.json"],
+        },
+        "mochitest-7": {
+            "category": "mochitest",
+            "extra_args": ["--total-chunks", "8", "--this-chunk", "7", "--run-only-tests", "androidx86.json"],
+        },
+        "mochitest-8": {
+            "category": "mochitest",
+            "extra_args": ["--total-chunks", "8", "--this-chunk", "8", "--run-only-tests", "androidx86.json"],
+        },
+        "mochitest-gl": {
+            "category": "mochitest",
+            "extra_args": ["--test-path", "content/canvas/test/webgl", "--run-only-tests", "androidx86.json"],
+        },
+        "reftest-1": {
+            "category": "reftest",
+            "extra_args": ["--total-chunks", "4", "--this-chunk", "1", os.path.join('tests', 'layout', 'reftests', 'reftest.list')],
+        },
+        "reftest-2": {
+            "category": "reftest",
+            "extra_args": ["--total-chunks", "4", "--this-chunk", "2", os.path.join('tests', 'layout', 'reftests', 'reftest.list')],
+        },
+        "reftest-3": {
+            "category": "reftest",
+            "extra_args": ["--total-chunks", "4", "--this-chunk", "3", os.path.join('tests', 'layout', 'reftests', 'reftest.list')],
+        },
+        "reftest-4": {
+            "category": "reftest",
+            "extra_args": ["--total-chunks", "4", "--this-chunk", "4", os.path.join('tests', 'layout', 'reftests', 'reftest.list')],
+        },
+        "crashtest": {
+            "category": "reftest",
+            "extra_args": [os.path.join('tests', 'testing', 'crashtest', 'crashtests.list')]
+        },
+        "xpcshell": {
+            "category": "xpcshell",
+            "extra_args": ["--manifest", "xpcshell/xpcshell_android.ini"]
+        },
+        "robocop-1": {
+            "category": "mochitest",
+            "extra_args": ["--total-chunks", "2", "--this-chunk", "1", "--robocop-path=.", "--robocop-ids=fennec_ids.txt", "--robocop=robocop.ini"],
+        },
+        "robocop-2": {
+            "category": "mochitest",
+            "extra_args": ["--total-chunks", "2", "--this-chunk", "2", "--robocop-path=.", "--robocop-ids=fennec_ids.txt", "--robocop=robocop.ini"],
+        },
+        "jsreftest-1": {
+            "category": "reftest",
+            "extra_args": ["--total-chunks", "3", "--this-chunk", "1",
+                os.path.join('..','jsreftest', 'tests', 'jstests.list'),
+                "--extra-profile-file=jsreftest/tests/user.js" ]
+        },
+        "jsreftest-2": {
+            "category": "reftest",
+            "extra_args": ["--total-chunks", "3", "--this-chunk", "2",
+                os.path.join('..','jsreftest', 'tests', 'jstests.list'),
+                "--extra-profile-file=jsreftest/tests/user.js" ]
+        },
+        "jsreftest-3": {
+            "category": "reftest",
+            "extra_args": ["--total-chunks", "3", "--this-chunk", "3",
+                os.path.join('..','jsreftest', 'tests', 'jstests.list'),
+                "--extra-profile-file=jsreftest/tests/user.js" ]
+        },
+    }, # end of "test_definitions"
+    "suite_definitions": {
+        "mochitest": {
+            "run_filename": "runtestsremote.py",
+            "options": ["--autorun", "--close-when-done", "--dm_trans=sut",
+                "--console-level=INFO", "--app=%(app)s", "--remote-webserver=%(remote_webserver)s",
+                "--xre-path=%(xre_path)s", "--utility-path=%(utility_path)s",
+                "--deviceIP=%(device_ip)s", "--devicePort=%(device_port)s",
+                "--http-port=%(http_port)s", "--ssl-port=%(ssl_port)s",
+                "--certificate-path=%(certs_path)s", "--symbols-path=%(symbols_path)s"
+            ],
+        },
+        "reftest": {
+            "run_filename": "remotereftest.py",
+            "options": [ "--ignore-window-size", "--remote-webserver=%(remote_webserver)s",
+                "--xre-path=%(xre_path)s", "--deviceIP=%(device_ip)s",
+                "--devicePort=%(device_port)s", "--http-port=%(http_port)s",
+                "--ssl-port=%(ssl_port)s", "--httpd-path", "reftest/components"
+            ],
+        },
+        "xpcshell": {
+            "run_filename": "remotexpcshelltests.py",
+            "options": ["--deviceIP=%(device_ip)s", "--xre-path=%(xre_path)s",
+                "--testing-modules-dir=%(modules_dir)s", "--apk=%(installer_path)s",
+                "--no-logfiles"
+            ],
+        },
+    }, # end of "suite_definitions"
+}
