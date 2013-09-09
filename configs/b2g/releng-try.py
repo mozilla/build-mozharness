3849167dc7c29c414519a2e00ff8ba3f0f70b632
Rail Aliiev <rail@mozilla.com>
2013-09-08 21:14:16 -0400

    Bug 913834 - Bump b2g_try_emulator-jb_dep disk space requirements. r=philor
    --HG--
    branch : production
    extra : transplant_source : l%06%B5%CA%D3%40%DE3%B6%B7%A7v%01%24n%F8l%27%88%20

--- configs/b2g/releng-try.py
+++ configs/b2g/releng-try.py
@@ -35,7 +35,7 @@ config = {
         "GAIA_OPTIMIZE": "1",
         "WGET_OPTS": "-c -q",
     },
-    "purge_minsize": 15,
+    "purge_minsize": 20,
     #"clobberer_url": "http://clobberer-stage.pvt.build.mozilla.org/index.php",
     #"clobberer_url": "http://clobberer.pvt.build.mozilla.org/index.php",
     "is_automation": True,
