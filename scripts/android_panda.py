eb7e2185a9ff089f59a72be44ebcd088d71590b3
Dan Minor <dminor@mozilla.com>
2013-09-26 07:24:37 -0400

    Bug 892569 - Run remote C++ unit tests from test package; r=kmoir

--- scripts/android_panda.py
+++ scripts/android_panda.py
@@ -26,7 +26,7 @@ from mozharness.mozilla.testing.device import SUTDeviceMozdeviceMixin
 from mozharness.mozilla.testing.testbase import TestingMixin, testing_config_options
 from mozharness.mozilla.testing.unittest import DesktopUnittestOutputParser
 
-SUITE_CATEGORIES = ['mochitest', 'reftest', 'crashtest', 'jsreftest', 'robocop', 'xpcshell', 'jittest']
+SUITE_CATEGORIES = ['mochitest', 'reftest', 'crashtest', 'jsreftest', 'robocop', 'xpcshell', 'jittest', 'cppunittest']
 
 
 class PandaTest(TestingMixin, MercurialScript, VirtualenvMixin, MozpoolMixin, BuildbotMixin, SUTDeviceMozdeviceMixin):
@@ -115,6 +115,14 @@ class PandaTest(TestingMixin, MercurialScript, VirtualenvMixin, MozpoolMixin, Bu
                     "Suites are defined in the config file\n."
                     "Examples: 'jittest'"}
          ],
+        [['--cppunittest-suite', ], {
+            "action": "extend",
+            "dest": "specified_cppunittest_suites",
+            "type": "string",
+            "help": "Specify which cpp unittest suite to run. "
+                    "Suites are defined in the config file\n."
+                    "Examples: 'cppunittest'"}
+         ],
         [['--run-all-suites', ], {
             "action": "store_true",
             "dest": "run_all_suites",
@@ -131,7 +139,10 @@ class PandaTest(TestingMixin, MercurialScript, VirtualenvMixin, MozpoolMixin, Bu
     virtualenv_modules = [
         'mozpoolclient',
         'mozcrash',
-        {'name': 'mozdevice', 'url': os.path.join('tests', os.path.join('mozbase', 'mozdevice'))}
+        {'name': 'mozdevice', 'url': os.path.join('tests', os.path.join('mozbase', 'mozdevice'))},
+        'mozinfo',
+        'mozlog',
+        'mozprocess'
     ]
 
     def __init__(self, require_config_file=False):
@@ -347,6 +358,8 @@ class PandaTest(TestingMixin, MercurialScript, VirtualenvMixin, MozpoolMixin, Bu
             dirs['abs_test_install_dir'], 'mochitest')
         dirs['abs_jittest_dir'] = os.path.join(dirs['abs_test_install_dir'], "jit-test", "jit-test")
         dirs['shutdown_dir'] = abs_dirs['abs_work_dir'].rsplit("/", 2)[0]
+        dirs['abs_cppunittest_dir'] = os.path.join(
+            dirs['abs_test_install_dir'], 'cppunittests')
         for key in dirs.keys():
             if key not in abs_dirs:
                 abs_dirs[key] = dirs[key]
@@ -395,7 +408,8 @@ class PandaTest(TestingMixin, MercurialScript, VirtualenvMixin, MozpoolMixin, Bu
             'http_port': http_port,
             'ssl_port':  ssl_port,
             'app_name':  self.app_name,
-            'apk_name':  self.filename_apk
+            'apk_name':  self.filename_apk,
+            'apk_path':  self.apk_path
         }
         if self.config['%s_options' % suite_category]:
             for option in self.config['%s_options' % suite_category]:
