b2ebfbc156ad94ddbbfb41cc29099c8d38a3fb4f
Andrew Halberstadt <ahalberstadt@mozilla.com>
2013-10-08 15:39:05 -0400

    Bug 924569 - Don't use tooltool xre.zip for b2g desktop mochitests, r=jgriffin

--- configs/b2g/desktop_automation_config.py
+++ configs/b2g/desktop_automation_config.py
@@ -3,7 +3,6 @@
 config = {
     # mozharness options
     "application": "b2g",
-    "xre_url": "http://runtime-binaries.pvt.build.mozilla.org/tooltool/sha512/cba263cef46b57585334f4b71fbbd15ce740fa4b7260571a9f7a76f8f0d6b492b93b01523cb01ee54697cc9b1de1ccc8e89ad64da95a0ea31e0f119fe744c09f",
     "tooltool_servers": ["http://runtime-binaries.pvt.build.mozilla.org/tooltool/"],
 
     "exes": {
@@ -47,7 +46,6 @@ config = {
         "--console-level=INFO", "--test-manifest=%(test_manifest)s",
         "--total-chunks=%(total_chunks)s", "--this-chunk=%(this_chunk)s",
         "--profile=%(gaia_profile)s", "--app=%(application)s", "--desktop",
-        "--utility-path=%(utility_path)s", "--xre-path=%(utility_path)s",
-        "--certificate-path=%(cert_path)s",
+        "--utility-path=%(utility_path)s", "--certificate-path=%(cert_path)s",
     ],
 }
