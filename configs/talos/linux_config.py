4790931a2c5e6456ef6c71114ae36745e968f358
Aki Sasaki <asasaki@mozilla.com>
2013-10-30 15:55:57 -0700

    oops, backout, meant to go to default
    --HG--
    branch : production

--- configs/talos/linux_config.py
+++ configs/talos/linux_config.py
@@ -34,8 +34,4 @@ config = {
     "python_webserver": False,
     "webroot": '/builds/slave/talos-slave/talos-data',
     "populate_webroot": True,
-    "default_blob_upload_servers": [
-         "https://blobupload.elasticbeanstalk.com",
-    ],
-    "blob_uploader_auth_file" : os.path.join(os.getcwd(), "oauth.txt"),
 }
