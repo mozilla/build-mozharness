4bad15a05696268fc2664369b8d2a0ced0b03c32
Massimo Gervasini <mgervasini@mozilla.com>
2013-11-06 18:33:47 +0100

    bug 819963 - Split up mochitest-bc on desktop into 30minute chunks - patch 1. r=rail
    --HG--
    extra : amend_source : c09f0f6d972655ee79f9708c530051b9ea9262e7

--- configs/unittests/win_unittest.py
+++ configs/unittests/win_unittest.py
@@ -77,6 +77,9 @@ config = {
         "plain5": ["--total-chunks=5", "--this-chunk=5", "--chunk-by-dir=4"],
         "chrome": ["--chrome"],
         "browser-chrome": ["--browser-chrome"],
+        "browser-chrome-1": ["--total-chunks=3", "--this-chunk=1"],
+        "browser-chrome-2": ["--total-chunks=3", "--this-chunk=2"],
+        "browser-chrome-3": ["--total-chunks=3", "--this-chunk=3"],
         "mochitest-metro-chrome": ["--browser-chrome", "--metro-immersive"],
         "a11y": ["--a11y"],
         "plugins": ['--setpref=dom.ipc.plugins.enabled=false',
@@ -97,7 +100,7 @@ config = {
     },
     "all_xpcshell_suites": {
         "xpcshell": ["--manifest=tests/xpcshell/tests/all-test-dirs.list",
-        "%(abs_app_dir)s/" + XPCSHELL_NAME]
+                     "%(abs_app_dir)s/" + XPCSHELL_NAME]
     },
     "all_cppunittest_suites": {
         "cppunittest": ['tests/cppunittests']
