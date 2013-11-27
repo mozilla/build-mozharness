da904409976a2b2f7914e0ead96cef1dc19bde8b
Aki Sasaki <asasaki@mozilla.com>
2013-11-27 15:04:30 -0800

    bug 944100 - add b2g26_v1_2f to gecko-dev. r=hwine

--- configs/vcs_sync/beagle.py
+++ configs/vcs_sync/beagle.py
@@ -108,6 +108,35 @@ config = {
             ],
         },
     }, {
+        "repo": "https://hg.mozilla.org/releases/mozilla-b2g26_v1_2f",
+        "revision": "default",
+        "repo_name": "mozilla-b2g26_v1_2f",
+        "targets": [{
+            "target_dest": "beagle/.git",
+            "vcs": "git",
+            "test_push": True,
+        }, {
+            "target_dest": "gitmo-beagle",
+        }, {
+            "target_dest": "github-beagle",
+            "tag_config": {
+                "tag_regexes": [
+                    "^B2G_",
+                ],
+            },
+        }],
+        "vcs": "hg",
+        "branch_config": {
+            "branches": {
+                "default": "b2g26_v1_2f",
+            },
+        },
+        "tag_config": {
+            "tag_regexes": [
+                "^B2G_",
+            ],
+        },
+    }, {
         "repo": "https://hg.mozilla.org/releases/mozilla-b2g18_v1_1_0_hd",
         "revision": "default",
         "repo_name": "mozilla-b2g18_v1_1_0_hd",
