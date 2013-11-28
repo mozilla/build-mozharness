03bfcb02d4b2eabb53985a0f95d3ca17fe12764a
Ben Hearsum <bhearsum@mozilla.com>
2013-11-28 13:52:16 -0500

    bug 944100: b2g 1.2f branch support. r=rail
    --HG--
    extra : rebase_source : c0da03a46196d1e57df8f85cfe7f15a61312a0eb

--- configs/vcs_sync/gecko-git.py
+++ configs/vcs_sync/gecko-git.py
@@ -70,6 +70,28 @@ config = {
             ],
         },
     }, {
+        "repo": "https://hg.mozilla.org/releases/mozilla-b2g26_v1_2f",
+        "revision": "default",
+        "repo_name": "mozilla-b2g26_v1_2f",
+        "targets": [{
+            "target_dest": "gecko-git/.git",
+            "vcs": "git",
+            "test_push": True,
+        }, {
+            "target_dest": "github-gecko-git",
+        }],
+        "vcs": "hg",
+        "branch_config": {
+            "branches": {
+                "default": "v1.2f",
+            },
+        },
+        "tag_config": {
+            "tag_regexes": [
+                "^B2G_",
+            ],
+        },
+    }, {
         "repo": "https://hg.mozilla.org/releases/mozilla-b2g18",
         "revision": "default",
         "repo_name": "mozilla-b2g18",
