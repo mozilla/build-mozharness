0e9adcd1362b5067222e447641ea85159db192f4
Aki Sasaki <asasaki@mozilla.com>
2014-01-28 17:45:49 -0800

    bug 963686 - beagle 1.3t branch. r=hwine
    --HG--
    branch : production
    extra : transplant_source : vb%AD%EDP%7D%09%10%2B%A6%85%A5%D2%BA%DA%7D%BF%D5P%1B

--- configs/vcs_sync/beagle.py
+++ configs/vcs_sync/beagle.py
@@ -137,6 +137,35 @@ config = {
             ],
         },
     }, {
+        "repo": "https://hg.mozilla.org/releases/mozilla-b2g28_v1_3t",
+        "revision": "default",
+        "repo_name": "mozilla-b2g28_v1_3t",
+        "targets": [{
+            "target_dest": "beagle/.git",
+            "vcs": "git",
+            "test_push": True,
+        }, {
+            "target_dest": "gitmo-beagle",
+        }, {
+            "target_dest": "github-beagle",
+            "tag_config": {
+                "tag_regexes": [
+                    "^B2G_",
+                ],
+            },
+        }],
+        "vcs": "hg",
+        "branch_config": {
+            "branches": {
+                "default": "b2g28_v1_3t",
+            },
+        },
+        "tag_config": {
+            "tag_regexes": [
+                "^B2G_",
+            ],
+        },
+    }, {
         "repo": "https://hg.mozilla.org/releases/mozilla-b2g18_v1_1_0_hd",
         "revision": "default",
         "repo_name": "mozilla-b2g18_v1_1_0_hd",
