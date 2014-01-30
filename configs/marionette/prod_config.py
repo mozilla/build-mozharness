6bfac722af8043aec552764ee54a98d2ff03aeaa
Ben Hearsum <bhearsum@mozilla.com>
2014-01-30 08:52:23 -0500

    bug 964411: Create hg share directory for gaia-central for test slaves. r=aki
    --HG--
    branch : production
    extra : transplant_source : %A3Y%01%F55-L%92j%40%D9%93%18%FEE%5E%1BF%8E%F7

--- configs/marionette/prod_config.py
+++ configs/marionette/prod_config.py
@@ -1,12 +1,15 @@
 # This is a template config file for marionette production.
 import os
 
+HG_SHARE_BASE_DIR = "/builds/hg-shared"
+
 config = {
     # marionette options
     "test_type": "browser",
     "marionette_address": "localhost:2828",
     "test_manifest": "unit-tests.ini",
 
+    "vcs_share_base": HG_SHARE_BASE_DIR,
     "exes": {
         'python': '/tools/buildbot/bin/python',
         'virtualenv': ['/tools/buildbot/bin/python', '/tools/misc-python/virtualenv.py'],
