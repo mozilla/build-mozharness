c57b42ea03283e04f303336a25e3125bbbccf716
Jonathan Griffin <jgriffin@mozilla.com>
2014-03-11 13:14:37 -0700

    Bug 979962 - Reorder args

--- scripts/gaia_integration.py
+++ scripts/gaia_integration.py
@@ -44,8 +44,9 @@ class GaiaIntegrationTest(GaiaTest):
 
         # run 'make node_modules' first, so we can separately handle
         # errors that occur here
-        cmd = ['NODE_MODULES_GIT_URL=https://git.mozilla.org/b2g/gaia-node-modules.git',
-               'make', 'node_modules']
+        cmd = ['make',
+               'node_modules',
+               'NODE_MODULES_GIT_URL=https://git.mozilla.org/b2g/gaia-node-modules.git']
         kwargs = {
             'cwd': dirs['abs_gaia_dir'],
             'output_timeout': 300,
