8c4630a2906949960dcaea6655a3444fe85e439c
Chris AtLee <catlee@mozilla.com>
2014-04-08 15:19:43 -0400

    Bug 993596 - Enable updates for tarako on 1.3t branch r=jhopkins

--- configs/b2g/releng-otoro.py
+++ configs/b2g/releng-otoro.py
@@ -55,6 +55,7 @@ config = {
             'mozilla-b2g26_v1_2': '1.2.0',
             'mozilla-b2g26_v1_2f': '1.2.1',
             'mozilla-b2g28_v1_3': '1.3.0',
+            'mozilla-b2g28_v1_3t': '1.3.0t',
             'mozilla-aurora': '1.4.0',
             'mozilla-central': '1.5.0',
         },
