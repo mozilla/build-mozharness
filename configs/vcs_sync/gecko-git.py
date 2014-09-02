6ac3ce189865bbe259ab7b6bf9a8c3dd2b45a97c
Rail Aliiev <rail@mozilla.com>
2014-09-02 11:03:32 -0400

    Bug 1029851 - Tracking bug for 02-sep-2014 migration work. new vcs sync: start syncing aurora to v2.1. r=hwine

--- configs/vcs_sync/gecko-git.py
+++ configs/vcs_sync/gecko-git.py
@@ -48,6 +48,28 @@ config = {
         "PATH": "%(PATH)s:/usr/libexec/git-core",
     },
     "conversion_repos": [{
+        "repo": "https://hg.mozilla.org/releases/mozilla-aurora",
+        "revision": "default",
+        "repo_name": "mozilla-aurora",
+        "targets": [{
+            "target_dest": "gecko-git/.git",
+            "vcs": "git",
+            "test_push": True,
+        }, {
+            "target_dest": "github-gecko-git",
+        }],
+        "vcs": "hg",
+        "branch_config": {
+            "branches": {
+                "default": "v2.1",
+            },
+        },
+        "tag_config": {
+            "tag_regexes": [
+                "^B2G_",
+            ],
+        },
+    }, {
         "repo": "https://hg.mozilla.org/releases/mozilla-b2g32_v2_0",
         "revision": "default",
         "repo_name": "mozilla-b2g32_v2_0",
