be99b481a0aff48c57571e7b4fa6c2753200e661
Peter Moore <pmoore@mozilla.com>
2014-11-24 17:12:00 +0100

    Bug 1061188 - Rename ffxbld_dsa to ffxbld_rsa since it is an rsa key, not a dsa key,r=rail

--- configs/single_locale/staging.py
+++ configs/single_locale/staging.py
@@ -1,7 +1,7 @@
 config = {
     "upload_env": {
         "UPLOAD_USER": "ffxbld",
-        "UPLOAD_SSH_KEY": "~/.ssh/ffxbld_dsa",
+        "UPLOAD_SSH_KEY": "~/.ssh/ffxbld_rsa",
         "UPLOAD_HOST": "dev-stage01.srv.releng.scl3.mozilla.com",
         "POST_UPLOAD_CMD": "post_upload.py -b %(branch)s-l10n -p firefox -i %(buildid)s --release-to-latest --release-to-dated",
         "UPLOAD_TO_TEMP": "1"
