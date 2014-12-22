cf74ae266f46e14996f1e096122ffc84d23ccbff
James Graham <james@hoppipolla.co.uk>
2014-12-22 10:41:43 +0000

    Bug 1025066 - Enable web-platform-tests script to access the certutils binary, r=armenzg
    --HG--
    extra : rebase_source : 70e19c2d0ab3d15c4f203bdf10a8fbd73138bdd0
    extra : amend_source : 9f6fe0e998b7088bb729fb705195e7430d0598b8

--- scripts/web_platform_tests.py
+++ scripts/web_platform_tests.py
@@ -139,6 +139,7 @@ class WebPlatformTest(TestingMixin, MercurialScript, BlobUploadMixin):
         str_format_values = {
             'binary_path': self.binary_path,
             'test_path': dirs["abs_wpttest_dir"],
+            'test_install_path': dirs["abs_test_install_dir"],
             'abs_app_dir': abs_app_dir,
             'abs_work_dir': dirs["abs_work_dir"]
             }
@@ -150,7 +151,8 @@ class WebPlatformTest(TestingMixin, MercurialScript, BlobUploadMixin):
     def download_and_extract(self):
         self.install_minidump_stackwalk()
         super(WebPlatformTest, self).download_and_extract(
-            target_unzip_dirs=["config/*",
+            target_unzip_dirs=["bin/*",
+                               "config/*",
                                "mozbase/*",
                                "marionette/*",
                                "web-platform/*"])
