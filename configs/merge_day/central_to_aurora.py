180f1e9adbc5be26777bac1c0df9dd2152b05032
Rail Aliiev <rail@mozilla.com>
2015-06-25 17:15:01 -0700

    Bug 1175533 - Tracking bug for June-29-2015 migration work. r=nthomas

--- configs/merge_day/central_to_aurora.py
+++ configs/merge_day/central_to_aurora.py
@@ -1,25 +1,58 @@
 config = {
     "log_name": "central_to_aurora",
 
-    "branding_dirs": [
-        "mobile/android/config/mozconfigs/android-api-11/",
-        "mobile/android/config/mozconfigs/android-api-9-10-constrained/",
-        "mobile/android/config/mozconfigs/android-x86/",
-    ],
-    "branding_files": ["debug", "l10n-nightly", "nightly"],
+    "replacements": [
+        # File, from, to
+        ("{}/{}".format(d, f),
+        "ac_add_options --with-branding=mobile/android/branding/nightly",
+        "ac_add_options --with-branding=mobile/android/branding/aurora")
+        for d in ["mobile/android/config/mozconfigs/android-api-11/",
+                  "mobile/android/config/mozconfigs/android-api-9-10-constrained/",
+                  "mobile/android/config/mozconfigs/android-x86/"]
+        for f in ["debug", "nightly", "l10n-nightly"]
+    ] + [
+        # File, from, to
+        ("{}/{}".format(d, f),
+        "ac_add_options --with-branding=browser/branding/nightly",
+        "ac_add_options --with-branding=browser/branding/aurora")
+        for d in ["browser/config/mozconfigs/linux32",
+                  "browser/config/mozconfigs/linux64",
+                  "browser/config/mozconfigs/win32",
+                  "browser/config/mozconfigs/win64",
+                  "browser/config/mozconfigs/macosx64"]
+        for f in ["debug", "nightly"]
+    ] + [
+        ("browser/config/mozconfigs/macosx-universal/nightly",
+        "ac_add_options --with-branding=browser/branding/nightly",
+        "ac_add_options --with-branding=browser/branding/aurora")
 
-    "profiling_files": [
-        "mobile/android/config/mozconfigs/android-api-11/nightly",
-        "mobile/android/config/mozconfigs/android-api-9-10-constrained/nightly",
-        "mobile/android/config/mozconfigs/android-x86/nightly",
-        "browser/config/mozconfigs/linux32/nightly",
-        "browser/config/mozconfigs/linux64/nightly",
-        "browser/config/mozconfigs/macosx-universal/nightly",
-        "browser/config/mozconfigs/win32/nightly",
-        "browser/config/mozconfigs/win64/nightly"
-    ],
-    "elf_hack_files": [
-        # Not necessary since bug 788974 landed.
+    ] + [
+        # File, from, to
+        ("{}/l10n-nightly".format(d),
+        "ac_add_options --with-l10n-base=../../l10n-central",
+        "ac_add_options --with-l10n-base=..")
+        for d in ["mobile/android/config/mozconfigs/android-api-11/",
+                  "mobile/android/config/mozconfigs/android-api-9-10-constrained/",
+                  "mobile/android/config/mozconfigs/android-x86/"]
+    ] + [
+        # File, from, to
+        (f, "ac_add_options --enable-profiling", "") for f in
+        ["mobile/android/config/mozconfigs/android-api-11/nightly",
+         "mobile/android/config/mozconfigs/android-api-9-10-constrained/nightly",
+         "mobile/android/config/mozconfigs/android-x86/nightly",
+         "browser/config/mozconfigs/linux32/nightly",
+         "browser/config/mozconfigs/linux64/nightly",
+         "browser/config/mozconfigs/macosx-universal/nightly",
+         "browser/config/mozconfigs/win32/nightly",
+         "browser/config/mozconfigs/win64/nightly"]
+    ] + [
+        # File, from, to
+        ("browser/confvars.sh",
+         "ACCEPTED_MAR_CHANNEL_IDS=firefox-mozilla-central",
+         "ACCEPTED_MAR_CHANNEL_IDS=firefox-mozilla-aurora"),
+        ("browser/confvars.sh",
+         "MAR_CHANNEL_ID=firefox-mozilla-central",
+         "MAR_CHANNEL_ID=firefox-mozilla-aurora"),
     ],
     "locale_files": [
         "browser/locales/shipped-locales",
