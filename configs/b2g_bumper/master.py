cc17a882a2e5dbc98a82c5aacd467458dd849f9e
Hal Wine <hwine@mozilla.com>
2016-01-11 12:15:48 -0800

    bug 1224903 bustage Backed out changeset 3078d9b64d4e; r=bustage
    --HG--
    extra : amend_source : 84331df78e7f8ea541c07d1c14e4f9d4f84f255d

--- configs/b2g_bumper/master.py
+++ configs/b2g_bumper/master.py
@@ -79,11 +79,6 @@ config = {
             'ignore_groups': ['darwin'],
             'manifest_file': 'shinano.xml',
         },
-        'aries-l': {
-            'ignore_projects': ['gecko'],
-            'ignore_groups': ['darwin'],
-            'manifest_file': 'sony-aosp-l.xml',
-        },
     },
     'repo_remote_mappings': {
         'https://android.googlesource.com/': 'https://git.mozilla.org/external/aosp',
