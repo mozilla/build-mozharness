27327fe1b737b2604599a397d47bcc305c7fbb51
Hal Wine <hwine@mozilla.com>
2016-01-22 15:00:59 -0800

    bug 1193011 - more debug - save all mapfiles to logs; r=me

--- scripts/vcs-sync/vcs_sync.py
+++ scripts/vcs-sync/vcs_sync.py
@@ -988,8 +988,7 @@ intree=1
                 # bug 1193011 says there are problems on occasion, independently
                 # check calculation of additions and save off mapfiles
                 dirs = self.query_abs_dirs()
-                for mapfile in ['delta_for_mapper',
-                    'published_to_mapper', 'complete_mapfile']:
+                for mapfile in [delta_for_mapper, published_to_mapper, complete_mapfile]:
                     self.copyfile(src=mapfile, dest='logs/')
                 import subprocess   # this is debug code
                 # from https://gist.github.com/edufelipe/1027906 because we're py 2.6
