0c470728704269127e20bea6e8c50f214fa1a42b
Vlad Ciobancai <vciobancai@mozilla.com>
2016-04-25 15:52:38 +0300

    Bug 1265799 - Disable b2g-inbound, r=kmoir

--- configs/vcs_sync/beagle.py
+++ configs/vcs_sync/beagle.py
@@ -736,14 +736,6 @@ config = {
             "project": "gecko-dev"
         },
     }, {
-        "repo": "https://hg.mozilla.org/integration/b2g-inbound",
-        "revision": "default",
-        "repo_name": "b2g-inbound",
-        "targets": [{
-            "target_dest": "beagle/.git",
-            "vcs": "git",
-            "test_push": True,
-        }, {
             "target_dest": "gitmo-beagle",
         }, {
             "target_dest": "github-beagle",
