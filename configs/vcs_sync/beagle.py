7d5598c72ea32c15b640f6b8cc02b3e1926b63fa
Dave House <dhouse@mozilla.com>
2017-03-29 07:13:39 -0700

    bug 1350696 - stuck, trying disable of failing gimo push for esr52. r=bustage
    --HG--
    extra : amend_source : 3370e492ed9092eedbc3e20baf6f65f640191380

--- configs/vcs_sync/beagle.py
+++ configs/vcs_sync/beagle.py
@@ -719,8 +719,8 @@ config = {
             "target_dest": "beagle/.git",
             "vcs": "git",
             "test_push": True,
-        }, {
-            "target_dest": "gitmo-beagle",
+        # }, {
+        #     "target_dest": "gitmo-beagle",
         }, {
             "target_dest": "github-beagle",
         }],
